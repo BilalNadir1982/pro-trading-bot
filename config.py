9028d817b2a1a66bde02dd3e36504da932909930
BilalNadir1982 <168363678+BilalNadir1982@users.noreply.github.com>
2026-04-27 00:01:17 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,13 +3,4 @@ import os
 TOKEN = os.getenv("TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
-COINS = {
-    "BTCUSDT": "bitcoin",
-    "ETHUSDT": "ethereum",
-    "SOLUSDT": "solana",
-    "BNBUSDT": "binancecoin",
-    "XRPUSDT": "ripple",
-    "ADAUSDT": "cardano"
-}
-
-SYMBOLS = list(COINS.keys())
+SYMBOLS = ["BTCUSDT","ETHUSDT","SOLUSDT","BNBUSDT","XRPUSDT","ADAUSDT"]
