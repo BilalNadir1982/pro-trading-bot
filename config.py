3256e3b25498c576c07b80ba153d9acb12e3a91b
BilalNadir1982 <168363678+BilalNadir1982@users.noreply.github.com>
2026-04-26 17:07:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,9 @@ import os
 TOKEN = os.getenv("TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
-SYMBOLS = ["BTCUSDT", "ETHUSDT", "SOLUSDT", "BNBUSDT", "XRPUSDT"]
+SYMBOLS = [
+    "BTCUSDT","ETHUSDT","SOLUSDT",
+    "BNBUSDT","XRPUSDT","ADAUSDT"
+]
+
 INTERVAL = "15m"
