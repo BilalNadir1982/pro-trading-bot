ac8783370fdf3a5e7e8c36e26bed5b542c2d3d44
Bilal Nadir öztürk <168363678+BilalNadir1982@users.noreply.github.com>
2026-05-06 23:37:26 +0300

    Delete modules directory

--- modules/scanner.py
+++
@@ -1,9 +0,0 @@
-from binance.client import Client
-from config import BINANCE_API_KEY, BINANCE_API_SECRET
-
-client = Client(BINANCE_API_KEY, BINANCE_API_SECRET)
-
-def get_top_symbols():
-    tickers = client.get_ticker()
-    usdt_pairs = [t["symbol"] for t in tickers if "USDT" in t["symbol"]]
-    return usdt_pairs[:100]
