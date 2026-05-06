6783de48afbc8800fc5285c95079523e88f51c2f
Bilal Nadir öztürk <168363678+BilalNadir1982@users.noreply.github.com>
2026-05-06 23:22:40 +0300

    Update filters.py

--- modules/filters.py
+++ modules/filters.py
@@ -1,1 +1,12 @@
+import time
 
+last_signal = {}
+
+def allow(symbol):
+    now = time.time()
+    if symbol in last_signal:
+        if now - last_signal[symbol] < 900:  # 15 min
+            return False
+
+    last_signal[symbol] = now
+    return True
